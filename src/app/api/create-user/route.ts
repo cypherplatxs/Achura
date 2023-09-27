a551b66fb1619bb874f771517ab5f8d3b6855f27
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-09-26 22:30:08 -0500

    feat: initial setup

---
+++ src/app/api/create-user/route.ts
@@ -0,0 +1,4 @@
+import { NextResponse } from 'next/server'
+export async function GET(){
+    return NextResponse.json({ hello: 'world'})
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Page
