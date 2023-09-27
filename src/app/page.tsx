8d2949994ec51671f1a35fd43d18c1ae10f99a40
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-27 00:21:14 -0500

    🦙

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='min-h-screen w-full flex flex-col gap-10 justify-center items-center'>
         <h1 className='text-8xl font-bold' >Achura</h1>
         <p className='text-9xl animate-bounce'>🦙</p>
-        <h2 className='text-4xl font-bold'>Empowering NGO's </h2>
+        <h2 className='text-4xl font-bold'>Empowering NGO&lsquo;s </h2>
     </main>
   )
 }
