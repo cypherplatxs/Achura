cd4ebcfc67326ecc385c41b71104848d905164ff
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-09-27 00:46:59 -0500

    feat: add txn-history endpoint

---
+++ src/app/api/txn-history/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+//TODO: implement this with a real database or Blockchain
+
+type Address = `0x${string}`
+
+type Txn = {
+    hash: string
+    to: Address
+    amount: number
+    date: string
+}
+
+type Histories = {
+  [address in Address]?: Txn[]
+}
+
+
+const histories: Histories = {
+  '0x1234': [
+    {
+        hash: '0x1234',
+        to: '0x1234',
+        amount: 1000,
+        date: '2021-09-01'
+    },
+    {
+        hash: '0x1234',
+        to: '0x1234',
+        amount: 1000,
+        date: '2021-09-01'
+    },
+    {
+        hash: '0x1234',
+        to: '0x1234',
+        amount: 1000,
+        date: '2021-09-01'
+    }
+  ]
+}
+
+export async function GET (req: NextRequest) {
+  try {
+    const headers = new Headers (req.headers)
+    const address = headers.get('Address')
+    if (!address) {
+      throw new Error('No address at headers')
+    }
+    return NextResponse.json(
+      { "txn-history": histories[address as Address] },
+      { status: 200 }
+    )
+  } catch (e: any) {
+    console.error(e)
+    return NextResponse.json(
+      { message: 'Bad request', error: e.message },
+      { status: 400 }
+    )
+  }
+}
