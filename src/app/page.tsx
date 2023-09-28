09a8c244dee8b6ddef00554211362d1f2b868570
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-27 23:15:32 -0500

    🦙 Card  🦙

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
 import Image from 'next/image'
 import Hero from '../components/landing/Hero'
+import CustomCard from '../components/landing/Card'
 
 export default function Home() {
   return (
     <main className='min-h-screen w-full flex flex-col gap-10 justify-center items-center'>
      <Hero />
+     <div className="flex">
+     
+      <CustomCard
+        title="🌾 Mayor Adopción "
+        subtitle="nextui.org"
+        description="Ayudamos a las ONG para que entren al mundo de la blockchain"
+        githubLink="https://github.com/nextui-org/nextui"
+      />
+    
+    <CustomCard
+        title="💱 Cripto t fiat "
+        subtitle="nextui.org"
+        
+        description="Cambia de cripto a tu fiat local sin salir de ACHURA"
+        githubLink="https://github.com/nextui-org/nextui"
+      />
+         <CustomCard
+        title="🦩 Uso simple"
+        subtitle="nextui.org"
+        
+        description="Solo necesitas tenr una wallet de NEAR "
+        githubLink="https://github.com/nextui-org/nextui"
+      />
+
+
+    </div>
+
+ 
     </main>
   )
 }
