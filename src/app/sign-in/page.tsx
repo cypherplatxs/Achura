b7c690fd0221960e8e63dc0f90f3c1f56fb70f8b
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-27 20:12:01 -0500

    nea ya

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -124,18 +124,18 @@ export default function Example() {
 
       
           <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-withe">
-                Donante  O  Usuario
+              <label htmlFor="user" className="block text-sm font-semibold leading-6 text-withe">
+                  Usuario U Organización
               </label>
               <div className="mt-2">
                 <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
+                  id="user"
+                  name="user"
+                  autoComplete="user"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
                   <option>Usuario</option>
-                  <option>Donante</option>
+                  <option>Organización</option>
               
                 </select>
               </div>
