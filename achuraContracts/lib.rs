1e274a1e5d9fdbd05010e77f095f13d8e087da6d
Carlos Rodríguez <juminstock@gmail.com>
2023-09-29 00:37:46 -0500

    Added transferal function

--- achuraContracts/lib.rs
+++ achuraContracts/lib.rs
@@ -1,53 +1,73 @@
-use near_sdk::{env, near_bindgen, AccountId, Balance, BorshStorageKey, Promise, Timestamp};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen, AccountId, Balance, Timestamp};
+// use near_sdk::collections::UnorderedMap;
+use near_sdk::serde::Serialize;
 
 // Definición de la estructura de datos para rastrear depósitos y retiros
 #[near_bindgen]
-#[derive(Default, BorshStorageKey, BorshSerialize, BorshDeserialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Transaction {
-    sender: AccountId,
-    timestamp: Timestamp,
-    value: Balance,
-    tx_ix: u64,
-    beneficiary: AccountId,
-    //donations: Mapping<AccountId, u128>
+  sender: AccountId,
+  balance: Balance,
+  timestamp: Timestamp,
+  amount: u64,
+  beneficiary: AccountId,
+  // transaction_ix: i64
 }
 
-// Definición de la estructura para el rastreo de transacciones 
+// Definición de la estructura para el rastreo de transacciones
 #[near_bindgen]
-#[derive(Default, BorshStorageKey, BorshSerialize, BorshDeserialize)]
-pub struct ProjectContract {
-    transactions: Vec<Transaction>,
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Transactions {
+    transactions: Vec<Transaction>
 }
+
+// Función para definir el estado inicial del contrato
+impl Default for Transaction {
+    fn default() -> Self {
+      Self {
+        sender: env::current_account_id(),
+        balance: env::account_balance(),
+        timestamp: env::block_timestamp(),
+        amount: Default::default(),
+        beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap()
+      }
+    }
+  }
 
 #[near_bindgen]
 impl Transaction {
-    // Función para realizar un depósito
-    pub fn deposit(&mut self, account_beneficiary: AccountId) {
-        let sender = env::predecessor_account_id();
-        let timestamp = env::block_timestamp();
-        let value = env::attached_deposit();
-        let tx_ix = self.transactions.len() as u64;
+    // Función para realizar una transferencia
+    pub fn transferal(&mut self, account_beneficiary: AccountId, amount_to_send: u128) {
+        self.sender = env::predecessor_account_id();
         let beneficiary = account_beneficiary;
+        let amount = amount_to_send;
 
-        let transaction = Transaction {
-            sender,
-            timestamp,
-            value,
-            tx_ix,
-            beneficiary,
-        };
+        // let transaction = Transaction {
+        //     sender,
+        //     balance,
+        //     timestamp,
+        //     amount,
+        //     beneficiary,
+        // };
 
-        self.transactions.push(transaction);
-    }
-
-    // // Función para realizar un retiro
-    // pub fn withdraw(&mut self) -> Promise {
-
-    // }
-
-    // Función para obtener el historial de transacciones
-    pub fn get_transaction_history(&self) -> Vec<Transaction> {
-        self.transactions.clone()
+        // self.transactions.push(Transaction);
     }
 }
+
+
+
+
+
+//     // // Función para realizar un retiro
+//     // pub fn withdraw(&mut self) -> Promise {
+
+//     // }
+
+//     // Función para obtener el historial de transacciones
+//     // pub fn get_transaction_history(&self) -> Vec<Transaction> {
+//     //     self.transactions.clone()
+//     // }
+// }
