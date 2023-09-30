1e45c84f1564f4922ec3e881a85a01ad31c4d8b8
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-09-30 11:07:54 -0500

    feat: add wallet conection login

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -1,50 +1,57 @@
 'use client'
-import React from 'react';
+import React from 'react'
 import Image from 'next/image'
-import Nav from '@/components/landing/NavBar';
-import Footer from '@/components/landing/Footer';
-import Link from 'next/link';
+import Nav from '@/components/landing/NavBar'
+import Footer from '@/components/landing/Footer'
+import Link from 'next/link'
+import { Button } from '@nextui-org/react'
+import useWallet from '@/hooks/useWallet'
 
 const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
+  e.preventDefault()
   //TODO: implementar lógica de submit
   // axios.post('/api/user' ...)
-};
+}
 
-export default function Example() {
+export default function Example () {
+  const { openWalletModal } = useWallet()
+
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 pt-40">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-         
-             <a href='#'><p className='text-9xl animate-bounce justify-center items-center text-center'>🦙</p></a>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-withe">
-           Log In ACHURA
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 pt-40'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <Link href='/'>
+            <p className='text-9xl animate-bounce justify-center items-center text-center'>
+              🦙
+            </p>
+          </Link>
+          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-withe'>
+            ACHURA
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-  
-            <div >
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-8 py-6 text-2xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-  >
-               Conecta tu Near Wallet
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-          👛 Aun no tienes{' '} 
-            <Link href="https://www.mynearwallet.com/" target='_blank'  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-             Near Wallet de Near?
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <Button
+            onPress={openWalletModal}
+            radius='md'
+            size='lg'
+            color='primary'
+            className='w-full text-white font-bold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          >
+            Conecta tu Near Wallet
+          </Button>
+          <p className='mt-10 text-center text-sm text-gray-500'>
+            👛 Aun no tienes{' '}
+            <Link
+              href='https://www.mynearwallet.com/'
+              target='_blank'
+              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            >
+              Near Wallet de Near?
             </Link>
           </p>
         </div>
       </div>
-      
     </>
   )
 }
