107fb9fcd69abc5a27a6dd908a6ba8370f730d3e
Carlos Rodríguez <juminstock@gmail.com>
2023-10-06 01:29:22 -0500

    api added

--- achuraContracts/lib.rs
+++ achuraContracts/lib.rs
@@ -1,10 +1,13 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen, AccountId, Balance, Timestamp, Promise};
 use near_sdk::serde::{Serialize, Deserialize};
+use near_sdk::collections::TreeMap;
+use schemars::JsonSchema;
+use std::vec::Vec;
 
 // Definición de la estructura de datos para rastrear depósitos y retiros
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Transaction {
   sender: AccountId,
@@ -18,9 +21,10 @@ pub struct Transaction {
 // Definición de la estructura para el rastreo de transacciones
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[derive(Default)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Transactions {
-    transactions: Vec<Transaction>
+    beneficiary_transactions: TreeMap<AccountId, Vec<Transaction>>,
 }
 
 // Función para definir el estado inicial del contrato
@@ -36,56 +40,86 @@ impl Default for Transaction {
     }
   }
 
+// #[near_bindgen]
+// impl Transaction {
+//     // Función para realizar una transferencia
+//     #[payable]
+//     pub fn transferal(&mut self, account_beneficiary: AccountId, amount_to_send: u128) {
+//         self.sender = env::predecessor_account_id();
+//         self.beneficiary = account_beneficiary;
+//         self.amount = amount_to_send;
+
+//         // Se crea una instancia de la estructura "Transaction"
+//         let transaction = Transaction {
+//             sender: self.sender.clone(),
+//             balance: self.balance,
+//             timestamp: self.timestamp,
+//             amount: self.amount,
+//             beneficiary: self.beneficiary.clone()
+//         };
+
+//         // Se crea una instancia de la estructura "Transactions"
+//         let mut transactions = Transactions {
+//             transactions: Vec::new(),
+//         };
+
+//         // Se agrega la instancia de "transaction"
+//         transactions.transactions.push(transaction);
+//     }
+
+//     // Función para realizar un retiro
+//     #[payable]
+//     pub fn withdraw(&mut self) -> Promise {
+//         let attached_amount = env::attached_deposit();
+//         let sender = env::predecessor_account_id();
+
+//         // let callback_id: AccountId = sender.clone();
+//         // let method_name = "on_withdraw_done".to_string();
+//         // let input: Vec<u8> = vec![];
+//         // let gas: Gas = near_sdk::Gas(30_000_000_000);
+//         // let attached_near: Gas = near_sdk::Gas(30_000_000_000);
+
+//         // let callback: Promise = env::promise_create(
+//         //     callback_id, &method_name, &input, attached_amount, gas);
+
+//         Promise::new(sender).transfer(attached_amount)
+//     }
+// }
+
 #[near_bindgen]
 impl Transaction {
     // Función para realizar una transferencia
     #[payable]
     pub fn transferal(&mut self, account_beneficiary: AccountId, amount_to_send: u128) {
-        self.sender = env::predecessor_account_id();
-        self.beneficiary = account_beneficiary;
-        self.amount = amount_to_send;
+      self.sender = env::predecessor_account_id();
+      self.beneficiary = account_beneficiary;
+      self.amount = amount_to_send;
 
-        // Se crea una instancia de la estructura "Transaction"
+        // Agregar la transacción al beneficiario
         let transaction = Transaction {
             sender: self.sender.clone(),
             balance: self.balance,
             timestamp: self.timestamp,
             amount: self.amount,
-            beneficiary: self.beneficiary.clone()
+            beneficiary: account_beneficiary, // Usar el beneficiario proporcionado
         };
-
-        // Se crea una instancia de la estructura "Transactions"
-        let mut transactions = Transactions {
-            transactions: Vec::new(),
-        };
-
-        // Se agrega la instancia de "transaction"
-        transactions.transactions.push(transaction);
+        let mut transactions = Transactions::default(env::current_account_id());
+        transactions.add_transaction(account_beneficiary, transaction);
     }
 
-    // Función para realizar un retiro
-    #[payable]
-    pub fn withdraw(&mut self) -> Promise {
-        let attached_amount = env::attached_deposit();
-        let sender = env::predecessor_account_id();
-
-        // let callback_id: AccountId = sender.clone();
-        // let method_name = "on_withdraw_done".to_string();
-        // let input: Vec<u8> = vec![];
-        // let gas: Gas = near_sdk::Gas(30_000_000_000);
-        // let attached_near: Gas = near_sdk::Gas(30_000_000_000);
-
-        // let callback: Promise = env::promise_create(
-        //     callback_id, &method_name, &input, attached_amount, gas);
-
-        Promise::new(sender).transfer(attached_amount)
-    }
 }
 
 #[near_bindgen]
 impl Transactions {
-    // Función para obtener el historial de transacciones
-    pub fn get_transaction_history(&self) -> Vec<Transaction> {
-        self.transactions.clone()
+    // Función para agregar una transacción al beneficiario
+    pub fn add_transaction(&mut self, beneficiary: AccountId, transaction: Transaction) {
+        let mut beneficiary_transactions = self.beneficiary_transactions.get(&beneficiary).unwrap_or_default();
+        beneficiary_transactions.push(transaction);
+        self.beneficiary_transactions.insert(&beneficiary, &beneficiary_transactions);
+    }
+
+    // Función para obtener el historial de transacciones de un beneficiario
+    pub fn get_transaction_history(&self, beneficiary: AccountId) -> Vec<Transaction> {
+        self.beneficiary_transactions.get(&beneficiary).unwrap_or_default()
     }
 }
