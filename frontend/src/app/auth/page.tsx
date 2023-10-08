66f9251568ec359c81a308ac2a1f327296c75022
cold-briu <andresf.xyz@gmail.com>
2023-10-08 14:54:10 -0500

    fix: signUp form refactor

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,13 +1,7 @@
 'use client'
-import React, { useContext } from 'react'
-import { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {
   Button,
-  Divider,
-  Input,
-  Select,
-  SelectItem,
-  Textarea,
   Modal,
   ModalContent,
   ModalHeader,
@@ -17,38 +11,35 @@ import {
 } from '@nextui-org/react'
 import { useWallet, useSignUp } from '@/hooks'
 import { User } from '@/types'
-import { UserContext } from '@/context/userContext'
+import { SignUpForm } from '@/components'
+import { PATHS } from '@/config'
+import { useRouter } from 'next/navigation'
 
 export default function Page() {
-  const [role, setRole] = useState<string>()
   const { handleSignUp, error } = useSignUp()
   const [isLoading, setIsLoading] = useState(false)
-  const { openWalletModal, accountId, disconnectWallet, accounts } = useWallet()
-  const userIsLogged = useContext(UserContext)
-
+  const { openWalletModal, accountId, accounts } = useWallet()
+  const router = useRouter()
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
-  // console.debug(accounts)
+  const handleOnSubmit = async (data: User) => {
 
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const data = new FormData(e.currentTarget)
-    data.append('accountId', accountId as string)
-    data.append('publicKey', (accounts[0].publicKey || 0).toString())
-    const object = Object.fromEntries(data.entries())
-    const userObject = object as unknown as User;
-    if (!accountId) {
-      onOpen()
-      return
+    if (accountId) {
+      data.accountId = accountId
     }
+    if (accounts[0].publicKey) {
+      data.publicKey = accounts[0].publicKey
+    }
+
     setIsLoading(true)
-
-    await handleSignUp(userObject)
-
+    const response = await handleSignUp(data)
     setIsLoading(false)
+
+    if (response && !error) {
+      router.replace(PATHS.DASHBOARD)
+    }
   }
 
-  console.debug('userIsLogged', userIsLogged)
   return (
     <main className='min-h-screen w-screen relative'>
       <div
@@ -64,120 +55,18 @@ export default function Page() {
         />
       </div>
 
-      <div className='min-h-[90vh] flex py-10 justify-center items-center flex-col w-full'>
-        <form
-          onSubmit={handleOnSubmit}
-          className='px-5 lg:px-0 lg:w-1/2 flex flex-col gap-5'
-        >
-          <p className='text-center text-8xl animate-pulse'> 🦙</p>
-          <h2 className='text-center font-bold text-4xl'>Join to Achura</h2>
-          {!userIsLogged && accountId && (
-            <>
-              <div className='grid lg:grid-cols-2 gap-5'>
-                <Select
-                  onSelectionChange={(e: any) => setRole(e.currentKey)}
-                  isRequired
-                  name='type'
-                  label='Select your role'
-                >
-                  <SelectItem key={'founder'} value={'founder'}>
-                    Founder
-                  </SelectItem>
-                  <SelectItem key={'organization'} value={'organization'}>
-                    Organization
-                  </SelectItem>
-                </Select>
-                <Input
-                  isRequired
-                  inputMode='text'
-                  label='Representative name'
-                  placeholder='John Doe'
-                  name='representativeName'
-                />
-                <Input
-                  isRequired
-                  inputMode='numeric'
-                  label='ID number'
-                  type='number'
-                  placeholder='922991...'
-                  name='representativeIdentificationNumber'
-                />
-                <Input
-                  isRequired
-                  inputMode='email'
-                  label='Email'
-                  placeholder='john.doe@example.com'
-                  name='representativeEmail'
-                />
-                <Input
-                  isRequired
-                  inputMode='text'
-                  label='Legal entity name'
-                  placeholder='XYZ Company'
-                  name='legalEntityName'
-                />
-                <Input
-                  isRequired
-                  inputMode='numeric'
-                  label='Legal description'
-                  placeholder='892702'
-                  name='legalDescription'
-                />
-                <Input
-                  isRequired
-                  inputMode='email'
-                  label='Legal Entity Email'
-                  placeholder='info@xyzcompany.com'
-                  name='legalEntityEmail'
-                />
-                <Input
-                  isRequired
-                  inputMode='text'
-                  label='Legal Entity address'
-                  placeholder='1234 Elm St, City, Country'
-                  name='legalEntityAddress'
-                />
-                <Input
-                  isRequired
-                  inputMode='numeric'
-                  label='Legal Entity tax registration number'
-                  placeholder='89809289'
-                  name='legalEntityTaxRegistrationNumber'
-                />
-                <Input
-                  isRequired
-                  inputMode='text'
-                  label='Legal Entity country'
-                  placeholder='CountryABC'
-                  name='legalEntityCountry'
-                />
-                <Textarea
-                  className='lg:col-span-2'
-                  isRequired
-                  inputMode='text'
-                  label='Description'
-                  placeholder='Some description'
-                />
-              </div>
-              <Divider />
-            </>
-          )}
-          {error && <p className='text-center'>{error}</p>}
-          {isLoading && <p className='text-center'>Loading...</p>}
-          {accountId && !userIsLogged ? (
-            <Button
-              isDisabled={isLoading || !role}
-              color='primary'
-              type='submit'
-            >
-              Authenticate
-            </Button>
-          ) : (
-            <Button onPress={openWalletModal} color='primary'>
-              Connect your wallet
-            </Button>
-          )}
-        </form>
+      <div className='min-h-[90vh] flex py-10 justify-center items-center flex-col w-full gap-5'>
+        <p className='text-center text-8xl animate-pulse'> 🦙</p>
+        <h2 className='text-center font-bold text-4xl'>Join to Achura</h2>
+        {accountId ? (
+          <SignUpForm handleOnSubmit={handleOnSubmit} />
+        ) : (
+          <Button onPress={openWalletModal} color='primary'>
+            Connect your wallet
+          </Button>
+        )}
+        {error && <p className='text-center'>{error}</p>}
+        {isLoading && <p className='text-center'>Loading...</p>}
       </div>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
---
+++ frontend/src/config/index.ts
@@ -0,0 +1,4 @@
+export const PATHS = {
+    DASHBOARD: '/dashboard',
+    AUTH: '/auth',
+}
