1281af4fb441d415e61e73d56cc133907f34037f
cold-briu <39354014+cold-briu@users.noreply.github.com>
2023-10-13 04:21:31 -0500

    feat: add-receiver-tx

--- achuraContracts/lib.rs
+++ achuraContracts/lib.rs
@@ -1,7 +1,7 @@
-use near_sdk::{env, near_bindgen, AccountId, Balance, Promise, Timestamp};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::serde::{Serialize, Deserialize};
 use near_sdk::collections::LookupMap;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, Balance, Promise, Timestamp};
 use schemars::JsonSchema;
 
 // Definición: Estructura de datos para crear transacciones
@@ -12,8 +12,8 @@ pub struct Transaction {
     sender: AccountId,
     beneficiary: AccountId,
     amount: u32,
-	timestamp: Timestamp,
-	balance: Balance
+    timestamp: Timestamp,
+    balance: Balance,
 }
 
 // Definición: Estructura de datos para el rastreo de transacciones
@@ -24,9 +24,9 @@ pub struct Transactions {
 }
 
 impl Default for Transactions {
-	fn default() -> Self {
-	Self::new()
-	}
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 #[near_bindgen]
@@ -44,29 +44,41 @@ impl Transactions {
         let amount: u32 = amount_to_send;
         let sender: AccountId = env::predecessor_account_id();
         let balance: Balance = env::account_balance();
-		let timestamp: Timestamp = env::block_timestamp();
+        let timestamp: Timestamp = env::block_timestamp();
         let beneficiary: AccountId = beneficiary_to_send;
-		let sender_clone: AccountId = sender.clone();
+        let sender_clone: AccountId = sender.clone();
 
         // Validación: Verifica al sender y sus fondos
         if sender != env::predecessor_account_id() {
             env::panic_str("Solo el propietario de los fondos puede realizar una transferencia.");
         } else if balance < amount.into() {
-                env::panic_str("Saldo insuficiente para realizar la transferencia.")
-            }
+            env::panic_str("Saldo insuficiente para realizar la transferencia.")
+        }
 
         let transaction = Transaction {
-            sender,
-            beneficiary,
+            sender: sender.clone(),
+            beneficiary: beneficiary.clone(),
             amount,
-			timestamp,
-			balance
+            timestamp,
+            balance,
+        };
+
+        let tx2 = Transaction {
+            sender,
+            beneficiary: beneficiary.clone(),
+            amount,
+            timestamp,
+            balance,
         };
 
         let mut sender_transactions = self.transactions.get(&sender_clone).unwrap_or_default();
         sender_transactions.push(transaction);
+        self.transactions
+            .insert(&sender_clone, &sender_transactions);
 
-        self.transactions.insert(&sender_clone, &sender_transactions);
+        let mut receiver_tx = self.transactions.get(&beneficiary).unwrap_or_default();
+        receiver_tx.push(tx2);
+        self.transactions.insert(&beneficiary, &receiver_tx);
     }
 
     // Función: Realiza retiros si se tiene saldo
@@ -74,14 +86,16 @@ impl Transactions {
         let sender = env::predecessor_account_id();
         let amount: u128 = env::account_balance();
         let balance: Balance = env::account_balance();
-		let timestamp: Timestamp = env::block_timestamp();
-		let sender_clone: AccountId = sender.clone();
+        let timestamp: Timestamp = env::block_timestamp();
+        let sender_clone: AccountId = sender.clone();
         let sender_clone2: AccountId = sender.clone();
         let sender_transactions = self.transactions.get(&sender).unwrap_or_default();
 
         // Calcula: Fondos disponibles para retirar
-		let total_amount: Balance = sender_transactions.iter()
-        .map(|t| Into::<Balance>::into(t.amount)).sum();
+        let total_amount: Balance = sender_transactions
+            .iter()
+            .map(|t| Into::<Balance>::into(t.amount))
+            .sum();
 
         // Calcula: Saldo para retirar
         if total_amount > 0 {
@@ -92,15 +106,16 @@ impl Transactions {
                 beneficiary: env::signer_account_id(),
                 amount: amount.try_into().unwrap(),
                 timestamp,
-                balance
+                balance,
             };
-    
-        let mut sender_transactions = self.transactions.get(&sender_clone).unwrap_or_default();
-        sender_transactions.push(transaction);
-    
-        self.transactions.insert(&sender_clone, &sender_transactions);
 
-        transfer_promise
+            let mut sender_transactions = self.transactions.get(&sender_clone).unwrap_or_default();
+            sender_transactions.push(transaction);
+
+            self.transactions
+                .insert(&sender_clone, &sender_transactions);
+
+            transfer_promise
         } else {
             env::panic_str("No hay fondos disponibles para retirar.");
         }
