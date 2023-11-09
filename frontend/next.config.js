fcea1f81e89968055e3c031c18a7b802ed02e108
Carlos Rodríguez <juminstock@gmail.com>
2023-11-08 22:40:21 -0500

    Added initial config for i18next module

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const { i18n } = require('./next-i18next.config')
+
+module.exports = {
+  i18n,
+}
+
 module.exports = nextConfig
